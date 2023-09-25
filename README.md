6f945ce566fb826197c5945f1cc51cf75e6ca25c
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-09-25 15:40:14 +0700

    Praktikum 3

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pemrograman Mobile Minggu 5
+
+Nama : Sabilla Luthfi Rahmadhan
+
+No Absen : 19
+
+Kelas : TI-3G
+
+---
+
+## Praktikum 3 : Membuat proyek
---
+++ android/app/src/main/kotlin/com/example/tugas_flutter_codelabs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas_flutter_codelabs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
